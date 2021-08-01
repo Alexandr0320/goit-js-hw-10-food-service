ea69f2844362a5266fd301053ebc8419d67956ec
Oleksandr Babych <alexanrbabitsch@gmail.com>
2021-08-01 11:16:34 +0300

    init
    init

---
+++ src/js/addCard.js
@@ -0,0 +1,14 @@
+
+const source = document.querySelector('#menu-template').innerHTML.trim();
+// // Импортируем шаблон
+// import cardsTemplate from '../template/card.hbs';
+// // Импортируем массив данных
+// import menu from '../menu.json'
+
+
+// // Применяем шаблон для массива данных
+// const markup = cardsTemplate(menu);
+
+// // Находим элемент в DOM и добавляем разметку
+// const menuRef = document.querySelector('.js-menu');
+// menuRef.insertAdjacentHTML('beforeend', markup);
---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+import changeTheme from './changeTheme'
+import '../images/sprite.svg'
+import '../styles.css'
+import './lazy.js'
+import menu from '../store/menu.json';
+import newStringEl from './addCard.js'
+
+
+
+
+newStringEl(menu)
+changeTheme()
